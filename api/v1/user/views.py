9caaa44eb373b348f7653a3f0391c202b525ff46
wobin1 <nathanielmusa3@gmail.com>
2023-10-02 21:04:54 +0100

    added urls

--- api/v1/user/views.py
+++ api/v1/user/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+
+class UserList(APIView):
+
+    def get(self, request):
+        # geting all data from the database
+        try:
+            user = CustomUser.objects.all()
+            serializer = UserSerializer(user, many=True)
+            print(serializer)
+            return Response(serializer.data)
+        except Exception as e:
+            return Response(str(e), status=400)
+
+
+class UserCreate(APIView):
+    # create user function
+    def post(self, request):    
+        request_data = request.data
+        print(request_data)
+
+        # serializes request data and save new user
+        try:
+            serializer = UserSerializer(data=request_data)
+            if serializer.is_valid(raise_exception=True):
+                serializer.save()
+            print("user saved")
+            return Response({"message": str(e)}, status=200)
+        except Exception as e:
+            print(f"printing exception {e}")
+            return Response({"message": str(e)}, status=400)
+
+    
--- core/settings.py
+++ core/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'api.v1.user',
+    'api.v1.authentication',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
